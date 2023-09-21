15acd4796e7b3dc98deed1b6be479394e6f25d30
ErikMejiaA <131417967+ErikMejiaA@users.noreply.github.com>
2023-09-21 09:36:56 -0500

    feat :memo: modificacionDto se agredo un cambio en el Dto de Medicamento

--- Backend/src/ApiProyecto/Dtos/Medicamento/MedicamentoXcompraXventaDto.cs
+++ Backend/src/ApiProyecto/Dtos/Medicamento/MedicamentoXcompraXventaDto.cs
@@ -1,6 +1,5 @@
 namespace ApiProyecto.Dtos.Medicamento;
-//using ApiProyecto.Dtos.MedicamentoCompra;
-//using ApiProyecto.Dtos.MedicamentoVenta;
+using ApiProyecto.Dtos;
 public class MedicamentoXcompraXventaDto
 {
     public int Id { get; set; }
@@ -8,7 +7,7 @@ public class MedicamentoXcompraXventaDto
     public double Precio { get; set; }
     public int Stock { get; set; }
     public DateTime FechaExpiracion { get; set; }
-    //public List<MedicamentoCompraDto> MedicamentosComprados { get; set; }
-    //public List<MedicamentoVentaDto> MedicamentosVendidos { get; set; }
+    public List<MedicamentoCompraDTO> MedicamentosComprados { get; set; }
+    public List<MedicamentoVentaDTO> MedicamentosVendidos { get; set; }
         
 }
