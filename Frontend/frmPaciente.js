85a9a7b4e9dfe1310a503bbcf670ececc7355915
tatiana <tatianacalderon10@gmail.com>
2023-09-25 13:02:39 -0500

    feat: :lipstick: se modificsron formularios

--- Frontend/frmPaciente.js
+++ Frontend/frmPaciente.js
@@ -18,30 +18,30 @@ class FrmPaciente extends HTMLElement {
                         <div class="form-group">
                             <h5 class="card-title">Datos personales</h5>
                             <div class="row">
-                                <div class="form-group mb-3 col-4">
+                                <div class="form-group mb-3 col-md-4">
                                     <label for="numeroIdentificacion" class="form-label">Numero de identificación</label>
                                     <input type="text" aria-label="First name" class="form-control" name="numeroIdentificacion">
                                 </div>    
-                                <div class="form-group mb-3 col-4">
+                                <div class="form-group mb-3 col-md-4">
                                     <label for="nombrePaciente" class="form-label">Nombre Paciente</label>
                                     <input type="text" aria-label="First name" class="form-control" name="nombrePaciente">
                                 </div>
-                                <div class="form-group mb-3 col-4">
+                                <div class="form-group mb-3 col-md-4">
                                     <label for="fechaNacimiento" class="form-label">Fecha de Nacimiento</label>
                                     <input type="date" aria-label="First name" class="form-control" name="fechaNacimiento">
                                 </div>    
                             </div>
                             <h5 class="card-title">Datos de contacto</h5>
                             <div class="row">
-                                <div class="form-group mb-3 col-4">
+                                <div class="form-group mb-3 col-md-4">
                                     <label for="correo" class="form-label">Correo</label>
                                     <input type="email" aria-label="First name" class="form-control" name="correo">
                                 </div>
-                                <div class="form-group mb-3 col-4">
+                                <div class="form-group mb-3 col-md-4">
                                     <label for="telefono" class="form-label">Telefono</label>
                                     <input type="text" aria-label="First name" class="form-control" name="telefono">
                                 </div>
-                                <div class="form-group mb-3 col-4">
+                                <div class="form-group mb-3 col-md-4">
                                     <label for="selectCiudad" class="form-label">Ciudad</label>
                                     <select class="custom-select" id="selectCiudad">
                                         <option value="0" selected>Seleccione una opcion</option>;
@@ -49,16 +49,29 @@ class FrmPaciente extends HTMLElement {
                                 </div>
                             </div>
                             <div class="row">
-                                <div class="form-group mb-3 col-5">
+                                <div class="form-group mb-3 col-md-5">
                                     <label for="direccion" class="form-label">Dirección</label>
-                                    <input type="text" aria-label="First name" class="form-control" name="direccion">
+                                    <div class="row">
+                                    <div class="col-3 pr-1">
+                                    <input type="text" aria-label="First name" class="form-control" name="direccion" placeholder="Tipo via">
+                                    </div>
+                                    <div class="col-3 pl-1 pr-1">
+                                    <input type="number" aria-label="First name" class="form-control" name="direccion" placeholder="numero">
+                                    </div>
+                                    <div class="col-3 pl-1 pr-1">
+                                    <input type="text" aria-label="First name" class="form-control" name="direccion" placeholder="letra">
+                                    </div>
+                                    <div class="col-3 pl-1 pr-1">
+                                    <input type="text" aria-label="First name" class="form-control" name="direccion" placeholder="sufijo cardinal">
+                                    </div>
+                                    </div>
                                 </div>
-                                <div class="form-group mb-3 col-4">
+                                <div class="form-group mb-3 col-md-4">
                                     <label for="barrio" class="form-label">Barrio</label>
                                     <input type="text" aria-label="First name" class="form-control" name="barrio">
                                 </div>
                                
-                                <div class="form-group mb-3 col-3">
+                                <div class="form-group mb-3 col-md-3">
                                     <label for="codigoPostal" class="form-label">Codigo Postal</label>
                                     <input type="text" aria-label="First name" class="form-control" name="codigoPostal">
                                 </div>
